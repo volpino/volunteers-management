78834c93898bf24acd4bc0de5ac4ce4f5113d2d5
Davide Kirchner <davide.kirchner@yahoo.it>
2011-06-05 00:38:29 +0200

    aggiunti urls

--- volunteers_management/volnet/views/views.py
+++ volunteers_management/volnet/views/views.py
@@ -8,10 +8,18 @@ from django.template import RequestContext
 
 
 def home(request):
+    #chiama funzione che assegna i volontari
     user = request.user
     if user.is_authenticated():
         return HttpResponseRedirect("/accounts/profile/")
     return render_to_response("base.html")
+#dalla home (cioe`:da profiles): redirects
+#    imbecille -> emergencies/overview/
+#    volontario -> se enroled: events/mytask/
+#                  else      : emergencies/overview/
+#    member  -> events/myevents/
+#    organization -> se ha emergenza aperta: events/overview/
+#                    altrimenti: emergencies/create/
 
 def about(request):
     user = request.user
